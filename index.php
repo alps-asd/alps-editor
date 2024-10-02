b843b56b6c92ebeb4c61508fbcc9b69b776d2541
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-10-02 11:09:18 +0900

    Add initial routing logic to index.php
    This commit introduces a new index.php file that handles basic routing based on the HTTP request method. It serves a static HTML file for GET requests and routes POST requests to an API script. This setup ensures that the server properly delegates requests to the appropriate handlers.

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// リクエストされたURIを取得します。
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+// もしリクエストされたファイルが実際に存在する場合は、そのままサーバーが処理します。
+if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
+    return false;
+}
+
+$script = match ($_SERVER["REQUEST_METHOD"]) {
+    'GET'=> __DIR__. '/htdocs/index.html',
+    'POST'=> __DIR__. '/api/index.php',
+};
+
+require_once $script;
